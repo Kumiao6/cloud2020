5a3632a1be722c399c743a4840afdb95b935fbbb
Kumiao6 <50994719+Kumiao6@users.noreply.github.com>
2022-04-03 12:00:45 +0800

    payment8001

---
+++ cloud-provider-payment8001/src/main/java/com/angenin/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.angenin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：m
+ * @date ：Created in 2022/4/2 21:08
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/angenin/springcloud/server/PaymentService.java
@@ -0,0 +1,24 @@
+package com.angenin.springcloud.server;
+
+import com.angenin.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author ：m
+ * @date ：Created in 2022/4/2 21:13
+ */
+@Service
+public interface PaymentService {
+
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+
+}
+
+
+
+
