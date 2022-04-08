aab67272d1d60e4a126dd64d14227933fd0cd261
Kumiao6 <50994719+Kumiao6@users.noreply.github.com>
2022-04-08 14:10:09 +0800

    77 config分布式配置中心动态刷新 terminal使用post动态刷新 curl  -X POST "http://localhost:3355/actuator/refresh"

---
+++ cloud-config-center-3355/src/main/java/com/angenin/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.angenin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ：m
+ * @date ：Created in 2022/4/8 11:46
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-center-3355/src/main/java/com/angenin/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.angenin.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：m
+ * @date ：Created in 2022/4/8 11:49
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")    //spring的@Value注解
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
