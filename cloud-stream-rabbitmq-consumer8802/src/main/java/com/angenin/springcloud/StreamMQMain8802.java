633de2fdbc370f41dbbf474ab70b93b973528490
Kumiao6 <50994719+Kumiao6@users.noreply.github.com>
2022-04-09 00:13:40 +0800

    91 Stream之间的重复消费和消息持久化

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/angenin/springcloud/StreamMQMain8802.java
@@ -0,0 +1,18 @@
+package com.angenin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：m
+ * @date ：Created in 2022/4/8 23:20
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+
+}
+
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/angenin/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.angenin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：m
+ * @date ：Created in 2022/4/8 23:40
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
